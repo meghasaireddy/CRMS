e4109e43b1931874279164b5a4ea365e9c46d2a0
meghasaireddy <d.meghasaireddy07055@gmail.com>
2025-11-17 11:35:45 +0530

    v1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import "../App.css";
+
+export default function Login() {
+  const [form, setForm] = useState({ username: "d.meghasaireddy07055@gmail.com", password: "Devi_reddy@070506" });
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+  
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/auth/login", form);
+      localStorage.setItem("token", res.data);
+      localStorage.setItem("username", form.username);
+      navigate("/dashboard");
+    } catch {
+      alert("Invalid credentials. Try again.");
+    }
+  };
+
+  return (
+    <div className="auth-container car-theme">
+      <div className="auth-box">
+        <h2>Car Rental Login</h2>
+        <form onSubmit={handleLogin}>
+          <input type="text" placeholder="Username" required
+                 onChange={(e) => setForm({ ...form, username: e.target.value })} />
+          <input type="password" placeholder="Password" required
+                 onChange={(e) => setForm({ ...form, password: e.target.value })} />
+          <button type="submit">Login</button>
+        </form>
+        <p>New user? <Link to="/signup">Register</Link></p>
+      </div>
+    </div>
+  );
+}
